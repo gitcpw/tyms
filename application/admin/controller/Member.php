77a846fc9e08f5a818031ff781ae8399a82aa8cf
gitcpw <17193402281@163.com>
2017-07-27 17:04:18 +0800

    时间：2017/07/27 星期四 17:01 备注：后台模块除报表和分销管理之外搭建完成（功能需要按照项目做修改）

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -1,16 +1,9 @@
 <?php
 /**
- * tpshop
+ * thinkcmf     会员管理、提现申请
  * ============================================================================
- * 版权所有 2015-2027 深圳搜豹网络科技有限公司，并保留所有权利。
- * 网站地址: http://www.tp-shop.cn
- * ----------------------------------------------------------------------------
- * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和使用 .
- * 不允许对程序代码以任何形式任何目的的再发布。
- * 采用TP5助手函数可实现单字母函数M D U等,也可db::name方式,可双向兼容
- * ============================================================================
- * Author: 当燃      
- * Date: 2015-09-09
+ * Author: yizukeji.cn
+ * Date: 2017/07/27 15:31
  */
 
 namespace app\admin\controller;
@@ -571,7 +564,8 @@ class Member extends Admin {
         $this->assign('show',$show);
         $this->assign('pager',$Page);
         $this->assign('list',$list);
-        C('TOKEN_ON',false);
+        config('TOKEN_ON',false);
+        $this->setmeta('提现申请');
         return $this->fetch();
     }
     /**
@@ -735,7 +729,7 @@ class Member extends Admin {
         $show  = $Page->show();
         $this->assign('show',$show);
         $this->assign('list',$list);
-        C('TOKEN_ON',false);
+        config('TOKEN_ON',false);
         return $this->fetch();
     }
 }
